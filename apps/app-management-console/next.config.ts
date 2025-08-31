2951e8950184235d46e5785e17eadc5ddcc34821
Bernardo Cabral <bernardo.cabral@outlook.com>
2025-08-31 17:47:19 +0100

    fix: fix nextjs config for stage deploys

--- apps/app-management-console/next.config.ts
+++ apps/app-management-console/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: '/app-management-console',
+  basePath: '/management-console',
 };
 
 export default nextConfig;
