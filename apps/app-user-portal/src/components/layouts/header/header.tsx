ba15775324c942e8a7509e27e7d5e154bccb2321
Bernardo Cabral <bernardo.cabral@outlook.com>
2025-09-01 16:21:19 +0100

    fix(user-portal): minor fix on redirect from logo

--- apps/app-user-portal/src/components/layouts/header/header.tsx
+++ apps/app-user-portal/src/components/layouts/header/header.tsx
@@ -2,6 +2,12 @@
 import { AppLogo } from "@/components/custom/app-logo";
 import { useManagementConsoleAccess } from "@/hooks/use-session";
 import { HelpIndicator } from "@repo/pkg-frontend-common-kit/components";
+import {
+  MANAGEMENT_CONSOLE_BASE_URL,
+  SERVICE_PORTAL_BASE_URL,
+  USER_PORTAL_BASE_URL,
+} from "@repo/pkg-frontend-common-kit/constants";
+import { getOriginUrl } from "@repo/pkg-frontend-common-kit/utils";
 import { ManagementConsoleAccess } from "@scaleits-solutions-gmbh/services";
 import { Lock } from "lucide-react";
 import { useTranslations } from "next-intl";
@@ -9,15 +15,9 @@ import Link from "next/link";
 import CompanySwitcher from "./company-switcher";
 import MobileHeader from "./mobile-header";
 import UserIndicator from "./user-indicator";
-import {
-  MANAGEMENT_CONSOLE_BASE_URL,
-  SERVICE_PORTAL_BASE_URL,
-  USER_PORTAL_BASE_URL,
-} from "@repo/pkg-frontend-common-kit/constants";
-import { getOriginUrl } from "@repo/pkg-frontend-common-kit/utils";
 
 export default function Header() {
-  const t = useTranslations('components.layout.header');
+  const t = useTranslations("components.layout.header");
   const managementConsoleAccess = useManagementConsoleAccess();
   const canAccessManagementConsole =
     managementConsoleAccess === ManagementConsoleAccess.Admin ||
@@ -29,22 +29,22 @@ export default function Header() {
       {/* Desktop Header */}
       <div className="hidden md:block p-4 bg-card border-b">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <Link href={getOriginUrl()}>
+          <Link href={getOriginUrl() + USER_PORTAL_BASE_URL}>
             <AppLogo customSize={48} />
           </Link>
           <div className="flex items-center gap-6">
             {canAccessManagementConsole ? (
               <Link href={getOriginUrl() + MANAGEMENT_CONSOLE_BASE_URL}>
-                {t('managementConsole')}
+                {t("managementConsole")}
               </Link>
             ) : (
               <div className="flex gap-2 text-muted-foreground items-center">
                 <Lock className="w-4 h-4" />
-                <span className="">{t('managementConsole')}</span>
+                <span className="">{t("managementConsole")}</span>
               </div>
             )}
             <Link href={getOriginUrl() + SERVICE_PORTAL_BASE_URL}>
-              {t('servicePortal')}
+              {t("servicePortal")}
             </Link>
             <CompanySwitcher />
             <div className="-ml-2">
